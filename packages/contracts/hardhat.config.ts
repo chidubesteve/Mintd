afd35948ac5dd33dc52c11dd8cab116488329256
Chidube Anike <anikechidube@gmail.com>
2026-02-01 19:41:10 +0000

    Made envs feature scoped

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -2,15 +2,8 @@ import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mo
 import hardhatVerify from "@nomicfoundation/hardhat-verify";
 import {  defineConfig } from "hardhat/config";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({
-  path: path.resolve(__dirname, "../../.env"),
-});
+dotenv.config();
 
 export default defineConfig({
   plugins: [hardhatToolboxMochaEthersPlugin, hardhatVerify],
