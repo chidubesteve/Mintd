b7ea21d4d44bdd9856a565ef1e9d6e1d377855df
Chidube Anike <anikechidube@gmail.com>
2026-02-05 20:40:11 +0000

    not logging host

--- apps/server/src/config/db.ts
+++ apps/server/src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const connectDB = async () => {
     try {
         const connection = await mongoose.connect(process.env.MONGODB_URI!);
-        console.log(`MongoDB connected: ${connection.connection.host}`);
+        console.log(`MongoDB connected`);
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error}`);
         process.exit(1);
